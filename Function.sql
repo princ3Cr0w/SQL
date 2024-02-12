17bc104bc75ac66da1127584314ee3bbe64f15fe
Marc N <151791276+princ3Cr0w@users.noreply.github.com>
2024-02-12 14:53:51 -0500

    Create Function.sql

---
+++ Function.sql
@@ -0,0 +1,27 @@
+## Functions:
+
+    COUNT: Returns the number of rows in a table.
+    SUM: Returns the sum of values in a column.
+    AVG: Returns the average of values in a column.
+    MIN: Returns the minimum value in a column.
+    MAX: Returns the maximum value in a column.
+
+  SELECT COUNT(*)
+FROM table_name;
+
+SELECT SUM(column_name)
+FROM table_name;
+
+SELECT AVG(column_name)
+FROM table_name;
+
+SELECT MIN(column_name)
+FROM table_name;
+
+SELECT MAX(column_name)
+FROM table_name;
+
+
+
+
+
