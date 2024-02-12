a75bf6892a64abdf366df52decba60df081324a8
Marc N <151791276+princ3Cr0w@users.noreply.github.com>
2024-02-12 14:46:59 -0500

    Create Cheat_Sheet.sql

---
+++ Cheat_Sheet.sql
@@ -0,0 +1,39 @@
+# SQL cheat sheet covering some common SQL commands and functions
+  
+  -- SELECT
+  
+SELECT column1, column2, ...
+FROM table_name
+WHERE condition
+ORDER BY column_name
+  
+-- INSERT Statement:
+
+INSERT INTO table_name (column1, column2, ...)
+VALUES (value1, value2, ...)
+
+-- UPDATE Statement:
+
+UPDATE table_name
+SET column1 = value1, column2 = value2, ...
+WHERE condition
+
+-- DELETE Statement:
+
+DELETE FROM table_name
+WHERE condition
+
+-- CREATE TABLE Statement:
+
+CREATE TABLE table_name (
+    column1 datatype,
+    column2 datatype,
+    ...
+)
+
+-- ALTER TABLE Statement:
+
+ALTER TABLE table_name
+ADD column_name datatype
+
+-- 
